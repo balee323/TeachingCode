063b43ad87443826cbc366384451bf566b663c4e
balee323 <brienzhere@yahoo.com>
2019-10-23 12:25:02 +0900

    Initial Commit to new branch and repo

---
+++ Console Applications/SimpleLoop/SimpleLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(args[0]);
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("press anykey to exit");
+            Console.ReadKey();
+            Environment.Exit(0);
+            //.net Core console applications now do not automatically close in debug mode, but this feature can be turned off.
+            // Tools->Options->Debugging->Automatically close the console when debugging stops (check box)
+        }
+    }
+}

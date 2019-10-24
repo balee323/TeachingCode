a0a7db17d8ade7e846560866b0307ee1b1dbeeb5
balee323 <brienzhere@yahoo.com>
2019-10-24 15:59:07 +0900

    Initial commit

---
+++ Console Applications/SimpleLoop/DataTypes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+
+            var valTypesClass = new ValueTypesClass();  //just make an instance so we can step through in debugger
+
+
+
+
+            //Enum Value type (class level)
+            var color1 = ValueTypesClass.ClassColors.green;
+
+            //Enum Value Type (NameSpace level)
+            var color2 = NameSpaceColors.yellow;
+
+            //lets test some functions:
+            valTypesClass.WriteClassOutColor(color1); //tests ValueTypesClass Enum member
+            valTypesClass.WriteClassOutColor(color2); //test overloaded method (notice not a class enum)
+
+
+
+            valTypesClass.WriteOutNameSpaceColor(color2); //tests NameSpace Enum member
+            //you notice this won't work as the parameter passed doesn't match method signature
+            //valTypesClass.WriteOutNameSpaceColor(color1); 
+
+
+            //lets test the object parameter passing
+            valTypesClass.WriteOutColor(color1);
+            valTypesClass.WriteOutColor(color2);
+            valTypesClass.WriteOutColor(ValueTypesClass.ClassColors.red);
+            valTypesClass.WriteOutColor(NameSpaceColors.purple);
+            valTypesClass.WriteOutColor(new object()); //blow up? Let us see!  (comment out 1st funtion, and uncomment 2nd one)
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            Environment.Exit(0);
+
+
+        }
+    }
+}
--- Console Applications/SimpleLoop/SimpleLoop/Program.cs
+++ Console Applications/SimpleLoop/SimpleLoop/Program.cs
@@ -7,14 +7,29 @@ namespace SimpleLoop
         static void Main(string[] args)
         {
 
-            Console.WriteLine(args[0]);
-            Console.WriteLine("Hello World!");
+            bool isContinued = true;
 
-            Console.WriteLine("press anykey to exit");
-            Console.ReadKey();
+            //this loop will run at least once.
+            do
+            {
+                Console.WriteLine(args[0]);
+                Console.WriteLine("Hello World!");
+
+                Console.WriteLine("Press \"y\" to repeat, or press any other key to exit");
+                var rawInputKeyObj = Console.ReadKey(); //let's look at this object
+                var input = rawInputKeyObj.KeyChar.ToString();
+                if (input.ToUpper() != "Y")
+                {
+                    isContinued = false;
+                }
+
+
+            } while (isContinued);
+
+             //.net Core console applications now do not automatically close in debug mode, but this feature can be turned off.
+             // Tools->Options->Debugging->Automatically close the console when debugging stops (check box)
+
             Environment.Exit(0);
-            //.net Core console applications now do not automatically close in debug mode, but this feature can be turned off.
-            // Tools->Options->Debugging->Automatically close the console when debugging stops (check box)
         }
     }
 }

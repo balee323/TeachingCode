1760b3c4ec701366338afb294b36032844d90bb0
briancode32@outlook.com <briancode32@outlook.com>
2020-04-12 18:51:54 +0900

    added game and other stuff

--- CSharpFundamentals/DataTypes/Program.cs
+++ CSharpFundamentals/DataTypes/Program.cs
@@ -4,7 +4,7 @@ namespace DataTypes
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //entry point
         {
 
             //foreach (string str in args)
@@ -13,9 +13,9 @@ namespace DataTypes
             //}
 
 
-   
+          //
 
-            int defaultValueInt = new int(); //automatically set to 0
+            int defaultValueInt = new int(); //automatically set to 0  
             int secondInt = 5;
 
             //Console.WriteLine(defaultValueInt);
@@ -25,6 +25,9 @@ namespace DataTypes
             float defaultValueFloat = new float(); //automatically set to
             float secondFloat = 1.0005f;
             float thirdFloat = 1.0005F; //same as lowercase f
+
+
+            double doubleNum = 0.0d;
 
             //Console.WriteLine(defaultValueFloat);
             //Console.WriteLine(secondFloat);
@@ -62,17 +65,17 @@ namespace DataTypes
             //WriteLineWithColor("1: " + boolVal1.ToString(), ConsoleColor.Red); //false (default)
 
 
-            //bool boolVal2 = true;
-            //WriteLineWithColor("2: " + boolVal2.ToString(), ConsoleColor.Green);  //true
+           // bool boolVal2 = true;
+           // WriteLineWithColor("2: " + boolVal2.ToString(), ConsoleColor.Green);  //true
 
             //var boolVal3 = boolVal2;
-            //WriteLineWithColor("3: " + boolVal3.ToString(), ConsoleColor.Green);  //true
+           // WriteLineWithColor("3: " + boolVal3.ToString(), ConsoleColor.Green);  //true
 
-            //var boolVal4 = 1 == 1;
-            //WriteLineWithColor("4: " + boolVal4.ToString(), ConsoleColor.Green);  //true
+          //  var boolVal4 = 1 == 1;  //true or false
+           // WriteLineWithColor("4: " + boolVal4.ToString(), ConsoleColor.Green);  //true
 
-            //var boolVal5 = 1 == 5;
-            //WriteLineWithColor("5: " + boolVal5.ToString(), ConsoleColor.Red);  //?
+           // var boolVal5 = 1 == 5;
+           // WriteLineWithColor("5: " + boolVal5.ToString(), ConsoleColor.Red);  //?
 
             //var boolVal6 = 8 != 9;
             //WriteLineWithColor("6: " + boolVal6.ToString(), ConsoleColor.Green); //?
@@ -83,14 +86,14 @@ namespace DataTypes
             //var boolVal8 = (8 < 9) && (7 > 6);
             //WriteLineWithColor("8: " + boolVal8.ToString(), ConsoleColor.Green);  //true
 
-            //var boolVal9 = (8 < 9) && (6 > 7);
-            //WriteLineWithColor("9: " + boolVal9.ToString(), ConsoleColor.Red);  //?
+           // var boolVal9 = (8 < 9) && (6 > 7);
+           // WriteLineWithColor("9: " + boolVal9.ToString(), ConsoleColor.Red);  //?
 
             //var boolVal10 = (8 < 9) || (7 > 6);
-            //WriteLineWithColor("10: " + boolVal10.ToString(), ConsoleColor.Green);  //true
+           // WriteLineWithColor("10: " + boolVal10.ToString(), ConsoleColor.Green);  //true
 
-            //var boolVal11 = (8 < 9) || (6 > 7);
-            //WriteLineWithColor("11: " + boolVal11.ToString(), ConsoleColor.Green);  //?
+           // var boolVal11 = (8 < 9) || (6 > 7);
+          //  WriteLineWithColor("11: " + boolVal11.ToString(), ConsoleColor.Green);  //?
 
 
 
@@ -112,21 +115,21 @@ namespace DataTypes
 
             //Seeing if 2 objects are the same
 
-            var obj2 = new object();
-            var obj3 = new object();
+            //var obj2 = new object();
+            //var obj3 = new object();
 
-            var boolVal13 = obj2.Equals(obj3);
-            WriteLineWithColor("13: " + boolVal13.ToString(), ConsoleColor.Red);  //false
-
-
-
-            var boolVal14 = obj2.Equals(obj2);
-            WriteLineWithColor("14: " + boolVal14.ToString(), ConsoleColor.Green);  //true
+            //var boolVal13 = obj2.Equals(obj3);
+            //WriteLineWithColor("13: " + boolVal13.ToString(), ConsoleColor.Red);  //false
 
 
-            obj2 = obj3;
-            var boolVal15 = obj2.Equals(obj3);
-            WriteLineWithColor("15: " + boolVal15.ToString(), ConsoleColor.Green);  //true
+
+            //var boolVal14 = obj2.Equals(obj2);
+            //WriteLineWithColor("14: " + boolVal14.ToString(), ConsoleColor.Green);  //true
+
+
+            //obj2 = obj3;
+            //var boolVal15 = obj2.Equals(obj3);
+            //WriteLineWithColor("15: " + boolVal15.ToString(), ConsoleColor.Green);  //true
 
 
             #endregion
---
+++ LiveShareTest/LiveShareTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveShareTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //can you see this?
+            //type here: yes I can
+
+            //This is awesome!
+            //We can type code together here?
+            //Oh yes!
+            //That is amazing!
+            //Thanks for helping with this today!
+            //no problem! We can use this for classes in the future for sure!
+
+            while (true)
+            {
+                //infinte loop
+                Console.WriteLine("Awesome");
+                Task.Delay(500).Wait();
+
+            }
+
+        }
+    }
+}
